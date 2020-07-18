37b31c236723642a1fa804f11c8b1734a41f0f69
sridharseenu17 <sridharseenu2717@gmail.com>
2020-07-18 11:08:45 +0530

    committed

---
+++ src/org/sum/Sum.java
@@ -0,0 +1,16 @@
+package org.sum;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		
+		for(int i=1;i<=100;i++) {
+			if(i%2!=0) {
+				sum= sum+i;
+			}
+		}
+		System.out.println("Sum of odd numbers is "+sum);
+	}
+}
